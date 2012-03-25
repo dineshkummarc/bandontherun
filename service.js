207ffd25d771cca9eb76b1dd61bd473d98972e8d
Andrew Clunis <aclunis@macadamian.com>
2012-03-25 00:02:27 -0400

    asdfsa asdfsdafsdaf lol

--- service.js
+++ service.js
@@ -5,13 +5,79 @@ var restify = require('restify');
 var http = require('http');
 var server = restify.createServer();
 
+var express = require('express');
+var Bird = require('bird')({
+  oauth_token : '',
+  oauth_token_secret : '',
+  callback: 'http://bandontherun.orospakr.ca/'
+});
+
+var app = express.createServer();
+
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({secret: "just-the-letter-a"}));
+
+app.get('/', function(req, res){
+  if (req.session.signedIn) {
+    res.send("Hi " + req.session.screen_name + " it's nice to see you signed in");
+  } else {
+    res.send('<a href="/login">login</a>');
+  }
+});
+
+app.get('/login', function(req, res){
+    Bird.login(req, function(err, oauth_token, oauth_token_secret, results){
+      if (err) {
+        //handle the error here if twitter returns an error
+        res.send(err);
+      } else {
+        //set 
+        req.session.oauth_token = oauth_token;
+        req.session.oauth_token_secret = oauth_token_secret;
+        res.redirect("https://twitter.com/oauth/authorize?oauth_token="+req.session.oauth_token);
+      }
+    });
+});
+
+app.get('/callback', function(req, res){
+    Bird.auth_callback(req, function(err, access_token, access_token_secret, data){
+      if (err) {
+        //handle the error here if twitter returns an error
+        res.send(err);
+      } else {
+        req.session.screen_name = data.screen_name;
+        req.session.access_token = access_token;
+        req.session.access_token_secret = access_token_secret;
+        req.session.signedIn = 1;
+
+        res.redirect('/');
+      }
+    });
+});
+
+app.get('/home_timeline', function(req, res){
+    var options = req.query || {};
+    options.include_entities = true;
+    Bird.home_timeline(req, options, function(err, data, response){
+      if (err) {
+        //handle the error here if twitter returns an error
+        res.send(err);
+      } else {
+        res.send(data);  
+      }
+    });
+});
+
+app.listen(8081);
+
 var static = require('node-static');
 
 var frontend_server = new(static.Server)('.');
 
 // Adding /games route to the web service
 server.get('/jams', function(req, res) {
-    var client = require('mysql').createClient({'host':'localhost','port':3306,'user':'band','password':'deadlock'});
+    var client = reuqire('mysql').createClient({'host':'localhost','port':3306,'user':'band','password':'deadlock'});
     client.query('USE band');
     getJams(client, res);
 });
@@ -34,14 +100,14 @@ server.listen(8080);
 
 getJams = function(client, res) {
     client.query(
-	'select j.id as id, j.title as title, j.artist as artist, ut.display_name as player from jams as j left join jams_needs as jn on j.id = jn.jam left join user_types as ut on jn.type = ut.id',
-	function selectCb(error, results, fields) {
-	    if (error) {
-		console.log('GetData Error: ' + error.message);
-		client.end();
-	     return;
-	    }
-      
+	    'select j.id as id, j.title as title, j.artist as artist, ut.display_name as player from jams as j left join jams_needs as jn on j.id = jn.jam left join user_types as ut on jn.type = ut.id',
+	    function selectCb(error, results, fields) {
+	        if (error) {
+		        console.log('GetData Error: ' + error.message);
+		        client.end();
+	            return;
+	        }
+            
 	    var element;
 	    var jams_players = {};
 	    var jams = [];
